60bbe83bb97109a6b34da0b9493bc9a69cd736c2
Sn1kerS20 <118538505+Sn1kerS20@users.noreply.github.com>
2022-12-13 17:52:32 +0200

    Добавьте файлы проекта.

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def sum(a, b):
+    return a + b
+a = input ("Enter a:")
+b = input ("Enter b:")
+s = int(a) + int(b)
+print("Suma =", s)
+
+def riz(a, b):
+    return a - b
+a = input ("Enter a:")
+b = input ("Enter b:")
+r = int(a) - int(b)
+print("Rizn =", r)
+
+def dob(a, b):
+    return a * b
+a = input ("Enter a:")
+b = input ("Enter b:")
+d = int(a) * int(b)
+print("Dobutok =", d)
+
+def Chas(a, b):
+    return a / b
+a = input ("Enter a:")
+b = input ("Enter b:")
+ch = int(a) / int(b)
+print("Chastka =", ch)
+
+def Kv(a):
+    return a**2
+a = input ("Enter a:")
+k = int(a)**2
+print("Kvad =", k)
+
